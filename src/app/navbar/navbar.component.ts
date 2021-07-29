b031f3eebd16d48bfc834a41cbe530b405e640ed
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-07-29 16:42:52 +0200

    Updated navbar colors.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,36 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  animations: [
+    trigger('slideLeft', [
+      transition(':enter', [
+        style({transform: 'translateX(-40%)'}),
+        animate('400ms ease-out', style({transform: 'translateX(0%)'}))
+      ])
+    ]),
+    trigger('slideRight', [
+      transition('void => *', [
+        style({transform: 'translateX(100%)'}),
+        animate('700ms ease-out', style({transform: 'translateX(0%)'}))
+      ])
+    ]),
+    trigger('slideTop', [
+      transition('void => *', [
+        style({transform: 'translateY(-100%)'}),
+        animate('300ms ease-out', style({transform: 'translateY(0%)'}))
+      ])
+    ]),
+    trigger('slideBottom', [
+      transition('void => *', [
+        style({transform: 'translateY(100%)'}),
+        animate('300ms ease-out', style({transform: 'translateY(0%)'}))
+      ])
+    ])
+  ]
 })
 export class NavbarComponent implements OnInit {
 
@@ -21,11 +48,11 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.mainTitle = this.randomizeMainTitle();
+    this.randomize();
   }
 
-  private randomizeMainTitle(): string {
-    
-    return this.titles[Math.floor(Math.random() * this.titles.length)]
+  private randomize(): void {
+    const randInt = Math.floor(Math.random() * this.titles.length);
+    this.mainTitle =  this.titles[randInt];
   }
 }
