4ca2c7563714fb6dd35ebe3dcde6685b345431dc
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-01-14 07:32:57 +0100

    Fixed transparent nav.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor () { }
+  private routerSub: Subscription;
+  public url: String = "";
+
+  constructor (private router: Router) {
+    // subscribe to router
+    this.routerSub = this.router.events.subscribe({
+      next: (data) => {
+        if (data instanceof NavigationEnd) {
+          console.log(data['url']);
+          this.url = data['url'];
+        }
+      }
+    })
+  }
 
   ngOnInit(): void {
     
+  }
+
+  ngOnDestroy(): void {
+    if(this.routerSub) {
+      this.routerSub.unsubscribe();
+    }
   }
 }
